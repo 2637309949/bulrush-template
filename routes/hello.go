3a16d77354efa476eecb7ac1f95e5fb75015f240
2637309949@qq.com <2637309949@qq.com>
2019-03-21 16:30:39 +0800

    .

--- routes/hello.go
+++ routes/hello.go
@@ -11,39 +11,40 @@ package routes
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/2637309949/bulrush-template/models"
+	"github.com/2637309949/bulrush-template/services"
 	"github.com/gin-gonic/gin"
 	"github.com/globalsign/mgo/bson"
-	"github.com/2637309949/bulrush-template/services"
-	"github.com/2637309949/bulrush-template/models"
 )
 
-func hello (r *gin.RouterGroup) {
-	r.GET("/ping", func (c *gin.Context) {
-		services.AddUsers([]interface {} {
+func hello(r *gin.RouterGroup) {
+	r.GET("/ping", func(c *gin.Context) {
+		services.AddUsers([]interface{}{
 			models.User{
-				Name: "double",
+				Name:     "double",
 				Password: "111111",
-				Age: 24,
+				Age:      24,
 			},
 		})
 		users := services.FindUsers(bson.M{"name": "double"})
 		c.JSON(http.StatusOK, users)
 	})
 
-    /**
-     * @api {get} /hello 检验Token有效性
-     * @apiGroup Test
-     * @apiDescription 测试系统可用性
-     * @apiParam {String} accessToken        认证令牌
-     * @apiSuccessExample {json}             正常返回
-     * HTTP/1.1 200 OK
-     * {
-	 *        "message":    "ok"
-	 * }
-     */
-	r.GET("/hello", func (c *gin.Context) {
+	/**
+	     * @api {get} /hello 检验Token有效性
+	     * @apiGroup Test
+	     * @apiDescription 测试系统可用性
+	     * @apiParam {String} accessToken        认证令牌
+	     * @apiSuccessExample {json}             正常返回
+	     * HTTP/1.1 200 OK
+	     * {
+		 *        "message":    "ok"
+		 * }
+	*/
+	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": 	"ok",
+			"message": "ok",
 		})
 	})
 
