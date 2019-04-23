6cbce461c604e7e46c6633e67f7b7d8891d6de7a
double <2637309949@qq.com>
2019-04-23 11:22:07 +0800

    加载配置优化

--- conf/index.go
+++ conf/index.go
@@ -1,6 +1,6 @@
 /**
- * @author [author]
- * @email [example@mail.com]
+ * @author [Double]
+ * @email [2637309949@qq.com]
  * @create date 2019-01-16 20:40:52
  * @modify date 2019-01-16 20:40:52
  * @desc [description]
@@ -9,19 +9,50 @@
 package conf
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/2637309949/bulrush"
 	"github.com/2637309949/bulrush-template/utils"
+	"github.com/thoas/go-funk"
 )
 
-// ENV APP ENV
-var ENV = utils.Some(os.Getenv("ENV"), "local")
+var (
+	dir     = "conf/yaml"
+	eFlag   = flag.String("e", "", "specify environment variables")
+	envFlag = flag.String("env", "", "specify environment variables")
+	// ENV APP ENV
+	ENV = utils.Some(utils.Some(os.Getenv("ENV"), os.Getenv("env")), "local")
+	// CfgPath PATH
+	CfgPath string
+	// Cfg app cgf
+	Cfg *bulrush.Config
+)
 
-// CfgPath PATH
-var CfgPath = path.Join(".", fmt.Sprintf("conf/yaml/%s.yaml", ENV))
+func init() {
+	flag.Parse()
+	ENV = utils.Some(utils.Some(*eFlag, *envFlag), ENV)
+	envFileName := fmt.Sprintf("%s.yaml", ENV)
+	files, err := filepath.Glob(dir + "/**.yaml")
+	if err != nil {
+		panic(err)
+	}
+	envFiles := funk.Map(files, func(file string) string {
+		base := filepath.Base(file)
+		return base
+	}).([]string)
 
-// Cfg app cgf
-var Cfg = bulrush.NewCfg(CfgPath)
+	findFrom := funk.Find(envFiles, func(file string) bool {
+		return file == envFileName
+	})
+
+	if findFrom == nil {
+		panic(fmt.Errorf("envFileName %s from env or flag has no been found", envFileName))
+	} else {
+		CfgPath = path.Join(".", dir, envFileName)
+		Cfg = bulrush.NewCfg(CfgPath)
+	}
+}
