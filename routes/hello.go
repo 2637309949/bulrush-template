8448cf36eb121d0c353be313895409577cba6eeb
double <2637309949@qq.com>
2019-04-25 15:40:24 +0800

    add cache test

--- routes/hello.go
+++ routes/hello.go
@@ -11,14 +11,29 @@ package routes
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/2637309949/bulrush-template/models"
 	"github.com/2637309949/bulrush-template/services"
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 	"github.com/globalsign/mgo/bson"
 )
 
 func hello(router *gin.RouterGroup) {
+	store := persistence.NewInMemoryStore(time.Second)
+	/**
+	     * @api {get} /ping 数据库测试
+	     * @apiGroup Test
+	     * @apiDescription 测试系统可用性
+	     * @apiParam {String} accessToken        认证令牌
+	     * @apiSuccessExample {json}             正常返回
+	     * HTTP/1.1 200 OK
+	     * {
+		 *        "message":    "ok"
+		 * }
+	*/
 	router.GET("/ping", func(c *gin.Context) {
 		services.AddUsers([]interface{}{
 			models.User{
@@ -32,7 +47,7 @@ func hello(router *gin.RouterGroup) {
 	})
 
 	/**
-	     * @api {get} /hello 检验Token有效性
+	     * @api {get} /chache 缓存测试
 	     * @apiGroup Test
 	     * @apiDescription 测试系统可用性
 	     * @apiParam {String} accessToken        认证令牌
@@ -42,16 +57,10 @@ func hello(router *gin.RouterGroup) {
 		 *        "message":    "ok"
 		 * }
 	*/
-	router.GET("/hello", func(c *gin.Context) {
+	router.GET("/chache", cache.CachePage(store, time.Minute, func(c *gin.Context) {
+		fmt.Println("no chache")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "ok",
 		})
-	})
-
-	router.POST("/ptest", func(c *gin.Context) {
-		var json map[string]interface{}
-		if c.BindJSON(&json) == nil {
-			fmt.Print(json)
-		}
-	})
+	}))
 }
