a9c82da3b7bed8d3aaa937bcc7ab8763bc2e87a0
double <2637309949@qq.com>
2019-04-28 16:41:57 +0800

    FIXED

--- plugins.go
+++ plugins.go
@@ -51,10 +51,10 @@ func appUsePlugins(app bulrush.Bulrush) {
 		&logger.Logger{
 			Path: "logs",
 			Format: func(p *logger.Payload, ctx *gin.Context) string {
-				if p.Type == "IN" {
+				if p.Type == logger.INT {
 					startTime := time.Unix(p.StartUnix, 0).Format("2006/01/02 15:04:05")
 					return fmt.Sprintf("[%v bulrush] => %s %6s %s", startTime, p.IP, p.Method, p.URL)
-				} else if p.Type == "OUT" {
+				} else if p.Type == logger.OUT {
 					endOfTime := time.Unix(p.EndUnix, 0).Format("2006/01/02 15:04:05")
 					latency := float64(time.Unix(p.EndUnix, 0).Sub(time.Unix(p.StartUnix, 0)) / time.Millisecond)
 					return fmt.Sprintf("[%v bulrush] <= %.2fms %s %6s %s", endOfTime, latency, p.IP, p.Method, p.URL)
