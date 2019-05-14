f36ae83bb95aaa21db45e3e33187f10a118a32cd
double <2637309949@qq.com>
2019-05-14 16:43:50 +0800

    架构调整

--- index.go
+++ index.go
@@ -10,9 +10,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/2637309949/bulrush"
 	"github.com/2637309949/bulrush-template/conf"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -20,16 +22,17 @@ func main() {
 	app.Config(conf.CfgPath)
 	app.Inject("bulrushApp")
 	appUsePlugins(app)
-	app.Run(func(err error, config *bulrush.Config) {
-		if err != nil {
-			panic(err)
-		} else {
-			name := config.GetString("name", "")
-			port := config.GetString("port", "")
-			fmt.Println("\n\n================================")
-			fmt.Printf("App: %s\n", name)
-			fmt.Printf("Listen on %s\n", port)
-			fmt.Println("================================")
+	app.Run(func(httpProxy *gin.Engine, config *bulrush.Config) {
+		port := config.GetString("port", ":8080")
+		port = strings.TrimSpace(port)
+		name := config.GetString("name", "")
+		if prefix := port[:1]; prefix != ":" {
+			port = fmt.Sprintf(":%s", port)
 		}
+		fmt.Println("\n\n================================")
+		fmt.Printf("App: %s\n", name)
+		fmt.Printf("Listen on %s\n", port)
+		fmt.Println("================================")
+		httpProxy.Run(port)
 	})
 }
