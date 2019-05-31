2dbf87d612a026945189548388351f0707300507
double <2637309949@qq.com>
2019-05-31 14:27:06 +0800

    测试

--- index.go
+++ index.go
@@ -9,12 +9,8 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/2637309949/bulrush"
 	"github.com/2637309949/bulrush-template/conf"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -22,17 +18,5 @@ func main() {
 	app.Config(conf.CfgPath)
 	app.Inject("bulrushApp")
 	appUsePlugins(app)
-	app.Run(func(httpProxy *gin.Engine, config *bulrush.Config) {
-		port := config.GetString("port", ":8080")
-		port = strings.TrimSpace(port)
-		name := config.GetString("name", "")
-		if prefix := port[:1]; prefix != ":" {
-			port = fmt.Sprintf(":%s", port)
-		}
-		fmt.Println("\n\n================================")
-		fmt.Printf("App: %s\n", name)
-		fmt.Printf("Listen on %s\n", port)
-		fmt.Println("================================")
-		httpProxy.Run(port)
-	})
+	app.RunImmediately()
 }
