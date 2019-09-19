cb060d89b9197a66783a43e3c451eeaf41342f43
2637309949@qq.com <2637309949@qq.com>
2019-09-19 18:39:34 +0800

    styles: 代码规范

--- grpc/srv/sendmessage.go
+++ grpc/srv/sendmessage.go
@@ -8,6 +8,11 @@ import (
 // SendMessage is used to implement pb.SendMessage.
 type SendMessage struct{}
 
+// SendCode implements pb.SendCode
+func (s *SendMessage) SendCode(ctx context.Context, in *pb.CodeRequest) (*pb.CodeReply, error) {
+	return &pb.CodeReply{Status: pb.CodeReply_Success, Message: "send successfully"}, nil
+}
+
 // SendEmail implements pb.SendMessage
 func (s *SendMessage) SendEmail(ctx context.Context, in *pb.EmailRequest) (*pb.EmailReply, error) {
 	return &pb.EmailReply{Status: pb.EmailReply_Success, Message: "send successfully"}, nil
